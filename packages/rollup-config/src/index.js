e0bb50171b6082365832a148a7f6a919516717b9
Pedro Nauck <pedronauck@gmail.com>
2019-01-15 10:39:12 -0200

    chore: add terser to minify packages

--- packages/rollup-config/src/index.js
+++ packages/rollup-config/src/index.js
@@ -1,12 +1,34 @@
 const { omit, get } = require('lodash')
 const typescript = require('@pedronauck/rollup-plugin-typescript2')
 const babel = require('rollup-plugin-babel')
+const { terser } = require('rollup-plugin-terser')
 
+const isProd = process.env.NODE_ENV === 'production'
 const sizePlugin = require('./plugins/size')
 const copyPlugin = require('./plugins/copy')
 
 const defaultExternal = id =>
   !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/')
+
+const defaultPlugins = outputDir => [
+  babel({
+    exclude: 'node_modules/**',
+    runtimeHelpers: false,
+  }),
+  typescript({
+    rollupCommonJSResolveHack: true,
+  }),
+  isProd &&
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    }),
+  sizePlugin(outputDir),
+]
 
 const output = (format, outputDir, { plugins = [], external, ...opts }) => ({
   ...opts,
@@ -17,23 +39,12 @@ const output = (format, outputDir, { plugins = [], external, ...opts }) => ({
     chunkFileNames: `[name]${format !== 'cjs' ? '.[format]' : ''}.js`,
     entryFileNames: `[name]${format !== 'cjs' ? '.[format]' : ''}.js`,
   },
-  plugins: plugins.concat([
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: false,
-    }),
-    typescript({
-      rollupCommonJSResolveHack: true,
-    }),
-    sizePlugin(outputDir),
-  ]),
+  plugins: plugins.concat(defaultPlugins(outputDir).filter(Boolean)),
 })
 
 exports.copy = copyPlugin
 exports.config = (initial = {}) => {
   const outputDir = get(initial, 'outputDir', 'dist')
-  const optsInput = get(initial, 'input', [])
-  const input = Array.isArray(optsInput) ? optsInput : [optsInput]
   const opts = omit(initial, ['outputDir'])
   return [output('cjs', outputDir, opts), output('esm', outputDir, opts)]
 }
