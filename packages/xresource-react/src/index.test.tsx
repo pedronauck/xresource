c2f1fa66fb3c5bbd0a074943320ff9d95628baa4
Pedro Nauck <pedronauck@gmail.com>
2019-01-16 16:21:47 -0200

    chore(react): use new xresource api

--- packages/xresource-react/src/index.test.tsx
+++ packages/xresource-react/src/index.test.tsx
@@ -1,28 +1,52 @@
 import * as React from 'react'
-import { render, waitForElement } from 'react-testing-library'
+import { render, waitForElement, fireEvent } from 'react-testing-library'
 
 import basicResource from './fixtures/basic-resource'
 import { useResource } from './'
 
-test('using a basic resource', async () => {
+it('using a basic resource', async () => {
   const Foo = () => {
-    const { context, data } = useResource(basicResource)
-    return <div>{context.foo + data.bar}</div>
+    const { ctx, data } = useResource(basicResource)
+    return <div>{ctx.foo + data.bar}</div>
   }
 
   const { getByText } = render(<Foo />)
   await waitForElement(() => getByText('foofoobar'))
 })
 
-test('without update on read', async () => {
+it('without update on read', async () => {
   const Foo = () => {
-    const { context, data } = useResource(basicResource, {
+    const { ctx, data } = useResource(basicResource, {
       updateOnRead: false,
     })
 
-    return <div>{context.foo + data.bar}</div>
+    return <div>{ctx.foo + data.bar}</div>
   }
 
   const { getByText } = render(<Foo />)
   await waitForElement(() => getByText('fooundefined'))
+})
+
+test('changing context on click', async () => {
+  const Foo = () => {
+    const { ctx, setContext } = useResource(basicResource, {
+      updateOnRead: false,
+    })
+
+    return (
+      <React.Fragment>
+        <button data-testid="button" onClick={() => setContext({ foo: 'bar' })}>
+          Click
+        </button>
+        <span data-testid="text">{ctx.foo}</span>
+      </React.Fragment>
+    )
+  }
+
+  const { getByTestId, rerender } = render(<Foo />)
+  expect(getByTestId('text').textContent).toBe('foo')
+
+  fireEvent.click(getByTestId('button'))
+  rerender(<Foo />)
+  expect(getByTestId('text').textContent).toBe('bar')
 })
--- packages/xresource-react/src/index.ts
+++ packages/xresource-react/src/index.ts
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 import { Resource, ErrorMap, ResourceInstance } from 'xresource'
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-export interface UseResource<C, D> extends Omit<Resource<C, D>, 'effects'> {
-  context: C
+type OmitProps = 'effects' | 'onUpdateStart' | 'onUpdateDone'
+
+export interface UseResource<C, D> extends Omit<Resource<C, D>, OmitProps> {
+  ctx: C
   data: D
   error: ErrorMap<D>
   loading: boolean
@@ -14,40 +16,59 @@ export interface Opts {
   updateOnRead?: boolean
 }
 
+export interface State<C, D> {
+  ctx: C
+  data: D
+  error: ErrorMap<D>
+  loading: boolean
+}
+
 export function useResource<C = any, D = any>(
   instance: ResourceInstance<C, D>,
   opts: Opts = { updateOnRead: true }
 ): UseResource<C, D> {
   const resource = instance.read()
-  const { effects, ...rest } = resource
-  const [data, setData] = useState<D>(resource.getData())
-  const [context, setContext] = useState<C>(resource.getContext())
-  const [error, setError] = useState<ErrorMap<D>>(resource.getError())
-  const [loading, setLoading] = useState(false)
+  const {
+    effects,
+    context$,
+    data$,
+    error$,
+    onContextChange,
+    onUpdateDone,
+    onUpdateStart,
+  } = resource
+
+  const [state, setState] = useState<State<C, D>>({
+    ctx: context$.value,
+    data: data$.value,
+    error: error$.value,
+    loading: false,
+  })
 
   useEffect(() => {
-    const contextSub = resource.onContextChange(setContext)
-    const onStartSub = resource.onUpdateStart(() => setLoading(true))
-    const onDoneSub = resource.onUpdateDone((ctx, data, error) => {
-      setContext(ctx)
-      setData(data)
-      setError(error)
+    const onCtxSub = onContextChange(ctx => {
+      setState(s => ({ ...s, ctx }))
+    })
+
+    const onStartSub = onUpdateStart(() =>
+      setState(s => ({ ...s, loading: true }))
+    )
+
+    const onDoneSub = onUpdateDone((data, error) => {
+      setState(s => ({ ...s, data, error }))
     })
 
     opts.updateOnRead && resource.update()
     return () => {
-      contextSub()
+      onCtxSub()
       onStartSub()
       onDoneSub()
     }
   }, [])
 
   return {
-    ...rest,
+    ...resource,
     ...effects,
-    data,
-    context,
-    error,
-    loading,
+    ...state,
   }
 }
